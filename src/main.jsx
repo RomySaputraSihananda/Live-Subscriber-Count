093cf13b1b081fa44c2d2840e666330ae4de9a00
romys <romysaputrasihanandaa@gmail.com>
2023-10-30 23:40:55 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode basename="/Live-Subscriber-Count">
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Simple-CRUD-ReactZustand">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
