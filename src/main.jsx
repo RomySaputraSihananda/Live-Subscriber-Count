29a2126cd55fb04094b2a1425cce0421748796ae
romys <romysaputrasihanandaa@gmail.com>
2023-10-30 22:58:52 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/Live-Subscriber-Count">
     <App />
   </React.StrictMode>,
 )
